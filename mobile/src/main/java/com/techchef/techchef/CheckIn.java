7db9b23b49ae30d9dda24c43aced1b98159932f4
Mohammad AL-Rashed <alrashedmohammad91@gmail.com>
2015-03-15 23:13:00 +0300

    Design Check In Layout

--- mobile/src/main/java/com/techchef/techchef/CheckIn.java
+++ mobile/src/main/java/com/techchef/techchef/CheckIn.java
@@ -1,37 +1,39 @@
 package com.techchef.techchef;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainScreen extends ActionBarActivity {
+public class CheckIn extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_screen);
+        setContentView(R.layout.check_in);
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main_screen, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_check_in, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-        return super.onOptionsItemSelected(item);
-    }
 
-    public void showCustomerView(View v) {
-        Intent intent = new Intent(this,CheckIn.class);
-        startActivity(intent);
+        return super.onOptionsItemSelected(item);
     }
 }
